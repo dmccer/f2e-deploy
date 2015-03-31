8f20f4025213015c9e7824c1f17f57dd36069ceb
Kane <yunhua.xiao@guluauto.com>
2015-03-31 12:43:35 +0800

    f2e build

--- routes/f2e.js
+++ routes/f2e.js
@@ -8,5 +8,5 @@ module.exports = router;
 router.post('/alpha', function (req, res) {
   console.log('准备发布 alpha 环境...');
 
-  f2e_build_service(req.body, config.alpha_work_path);
+  f2e_build_service.build(req.body, config.alpha_work_path);
 });
