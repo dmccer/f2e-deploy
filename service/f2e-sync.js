bc1b8505720bad4bc2d7a74b352ee5ddb4583253
Kane <yunhua.xiao@guluauto.com>
2015-03-31 17:15:58 +0800

    save

--- service/f2e-sync.js
+++ service/f2e-sync.js
@@ -2,7 +2,7 @@ var path = require('path');
 var shell = require('shelljs');
 
 module.exports = function(proj_dir, static_server) {
-  var pkg = require(path.resolve(proj_dir, pkg.name, './package.json'));
+  var pkg = require(path.resolve(proj_dir, './package.json'));
   var deploy_dir = path.resolve(static_server, pkg.name, pkg.version);
 
   console.log(proj_dir, pkg.name, pkg.version, pkg.dest);
