14f3f394347becff316d2f2732760e3952e679ea
Kane <yunhua.xiao@guluauto.com>
2015-03-31 11:48:25 +0800

    body parser

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.post('/deploy', function (req, res) {
   console.log('req: ', req.body);
