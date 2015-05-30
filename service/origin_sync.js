e0c5ca64a282f9749fe3e544deb05b04d1459c78
Kane <dmccer@gmail.com>
2015-05-30 12:45:26 +0800

    a

--- service/origin_sync.js
+++ service/origin_sync.js
@@ -2,10 +2,11 @@ var path = require('path');
 var shell = require('shelljs');
 
 module.exports = function(src, dest_dir) {
-  return shell.exec('sudo mkdir -p ' + dest_dir).output +
-    shell.exec([
-      'sudo cp -rf',
-      src,
-      dest_dir
-    ].join(' '));
+  shell.exec('sudo mkdir -p ' + dest_dir);
+
+  return shell.exec([
+    'sudo cp -rf',
+    src,
+    dest_dir
+  ].join(' '));
 };
