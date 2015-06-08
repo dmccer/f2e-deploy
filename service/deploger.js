d8f5808278009facf24bb77f686e0849aff0df24
Kane <dmccer@gmail.com>
2015-06-08 13:40:53 +0800

    rm comment

--- service/deploger.js
+++ service/deploger.js
@@ -16,16 +16,3 @@ Deploger.prototype.errHandler = function(data) {
 };
 
 module.exports = Deploger;
-
-// 创建 log 文件，初始化 logger
-// deploger
-//   .on('before-deploy', function(data) {
-//     publish_logger = require('../logger')(path.join(data.log_dir, data.log_file), 'publish');
-//     publish_logger.trace('发布' + data.env + ' 环境中');
-//
-//     build_logger = log4js.getLogger('build');
-//   })
-//   .on('mkdir-log-err', errHandler.bind(null, logger))
-//   .on('touch-logfile-err', errHandler.bind(null, logger))
-//   .on('clear-logfile-err', errHandler.bind(null, logger))
-// ;
