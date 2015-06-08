c0133de8b7f066d8476aa0f901a96223fb51e8e3
Kane <dmccer@gmail.com>
2015-06-08 11:07:28 +0800

    fix origin sync test bug

--- test/origin-sync.js
+++ test/origin-sync.js
@@ -24,6 +24,12 @@ describe('service/origin-sync.js', function() {
     dest: 'dist'
   };
 
+  var dist = path.join(built.out_dir, pkg.dest);
+  var cssfile = path.join(dist, 'test.css');
+  shell.exec('mkdir -p ' + dist);
+  shell.exec('touch ' + cssfile);
+  shell.exec('echo "css content" > ' + cssfile);
+
   var logdir = 'log/f2e';
   var logfile = path.join(logdir, 'demigod.log');
 
@@ -34,11 +40,13 @@ describe('service/origin-sync.js', function() {
   var deploger = new Deploger('log/f2e/demigod.log', 'origin_sync');
 
   describe('module.exports()', function() {
+    var remote_group = path.resolve(config.static_server.alpha, built.repository.owner.username);
     var remote_dir = path.resolve(config.static_server.alpha, built.repository.owner.username, pkg.name);
     var dest_dir = path.join(remote_dir, pkg.version);
 
     afterEach(function() {
       deploger.removeAllListeners();
+      shell.exec('sudo rm -rf ' + remote_group);
     });
 
     it('should throw error and msg when mkdir failed', function(done) {
@@ -49,14 +57,12 @@ describe('service/origin-sync.js', function() {
 
       assert.throws(origin_sync.bind(null, deploger, pkg, built), '创建静态服务器目录' + dest_dir + '失败');
 
-      shell.exec('sudo rm -rf ' + remote_dir);
-
       done();
     });
 
     it('should throw err and msg when copy file failed', function() {
       var listener = function() {
-        shell.exec('sudo rm -rf ' + dest_dir);
+        shell.exec('sudo rm -rf ' + remote_dir);
       };
       deploger.on('after-mk-dest-dir', listener);
 
