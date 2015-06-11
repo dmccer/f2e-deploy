c631595e6e78fdfa39b91f27afd1b11e92be163b
Kane <dmccer@gmail.com>
2015-06-11 22:46:06 +0800

    submit something

--- api.js
+++ api.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 var download = require('./controller/download');
 var deploy = require('./controller/deploy');
+var logpool = require('./controller/logpool');
 
 module.exports = router;
 
 router.get('/alpha/:name', download);
 router.post('/alpha', deploy);
+router.post('/alpha/:name/log', logpool);
---
+++ controller/logpool.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var shell = require('shelljs');
+var _ = require('lodash');
+var config = require('../config');
+
+module.exports = function(req, res) {
+  var owner = _.trim(req.query.owner);
+  var name = _.trim(req.params.name);
+};
