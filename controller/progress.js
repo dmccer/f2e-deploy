84d95c6bbb7ef92840b5c2d552d359c958dc4fbf
Kane <dmccer@gmail.com>
2015-07-30 11:30:32 +0800

    progress

--- controller/progress.js
+++ controller/progress.js
@@ -1,11 +1,32 @@
 var path = require('path');
-var shell = require('shelljs');
 var _ = require('lodash');
 var config = require('../config');
+var Status = require('../service/status');
 
 module.exports = function(req, res) {
   var owner = _.trim(req.query.owner);
   var name = _.trim(req.params.name);
 
-  
+  Status.get({
+    name: name,
+    owner: owner
+  }, function(err, body) {
+    if (err) {
+      return res.status(500).json({
+        msg: err.message
+      });
+    }
+
+    var repos = body.data || {};
+
+    if (repos.status === -1) {
+      return res.status(200).json({
+        fail: true
+      });
+    }
+
+    return res.status(200).json({
+      progress: Math.floor(repos.status / 7 * 100)
+    });
+  });
 };
