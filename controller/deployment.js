049b6b525dbd2d4a911aa769e722d16eddc9402c
Kane <dmccer@gmail.com>
2015-08-10 23:52:15 +0800

    deploy

--- controller/deployment.js
+++ controller/deployment.js
@@ -5,54 +5,69 @@
  */
 var deployment = require('../model/deployment');
 var repo = require('../model/repo');
+var env = require('../model/env');
 var Deployer = require('../service/deployer');
 
 module.exports = function(req, res) {
-  var deployment_id = req.params.deployment_id;
+  var repo_id = req.params.repo_id;
 
   try {
-    deployment.findById(deployment_id, function(err, _deployment) {
+    deployment.findOne({
+      repo_id: repo_id,
+      branch: req.body.branch
+    }, function(err, _deployment) {
       if (err) {
         throw err;
       }
 
       // 发布中
-      if (doc.status === 1) {
+      if (_deployment.status === 1) {
         return res.status(200).json({
           status: _deployment.status,
           progress: _deployment.progress
         });
       }
 
-      repo.findById(_deployment.repo_id, function(err, _repo) {
+      env.findById(req.body.env_id, function(err, _env) {
         if (err) {
           throw err;
         }
 
-        // 发布
-        // repo_id, name, username, url, branch, after, suffix, env
-        var deployer = new Deployer({
-          repo_id: _deployment.repo_id,
-          branch: _deployment.branch,
-          after: _deployment.after,
-          name: _repo.name,
-          username: _repo.owner.username,
-          url: _repo.url,
-          env: _deployment.env.alias
-        });
+        _deployment.env.id = _env._id;
+        _deployment.env.alias = env.alias;
 
-        deployer
-          .run()
-          .then(function() {
-            res.status(200).json({
-              complete: true
-            });
-          })
-          .catch(function(err) {
-            res.status(500).json({
-              err: err.message
-            });
+        _deployment.save();
+
+        repo.findById(repo_id, function(err, _repo) {
+          if (err) {
+            throw err;
+          }
+
+          // 发布
+          // repo_id, name, username, url, branch, after, suffix, env
+          var deployer = new Deployer({
+            repo_id: _deployment.repo_id,
+            branch: _deployment.branch,
+            after: _deployment.after,
+            name: _repo.name,
+            username: _repo.owner.username,
+            url: _repo.url,
+            env: _env.alias
           });
+
+          deployer
+            .run()
+            .then(function() {
+              res.status(200).json({
+                complete: true
+              });
+            })
+            .catch(function(err) {
+              res.status(500).json({
+                err: err.message
+              });
+            });
+        });
       });
     });
   } catch (e) {
