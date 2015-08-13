41fffcca25b83ca829505a65ac51dc4ee1f2f1dc
Kane <dmccer@gmail.com>
2015-08-13 17:15:19 +0800

    progress js

--- static/js/progress.js
+++ static/js/progress.js
@@ -4,38 +4,67 @@
 $(function() {
   var publishing = false;
 
-  var $progress = $('[eid="progress"]');
-
   $('[eid="publish-btn"]').on('click', function(e) {
     e.preventDefault();
+    e.stopPropagation();
 
     if (publishing) {
       return;
     }
 
     publishing = true;
-    var repo = $(this).attr('repo');
-    var url = '/f2e/alpha/' + repo + '/progress';
+    var $progress = $(this).parents('li').find('[eid="progress"]');
+    var repo_id = $(this).attr('repo_id');
+    var env_id = $(this).attr('env_id');
+    var env_name = $(this).attr('env_name');
+    var env_alias = $(this).attr('env_alias');
+    var branch = $(this).attr('branch');
+    var owner = $(this).attr('owner');
+    var repo_name = $(this).attr('repo_name');
 
-    //$.ajax({
-    //  url: '/f2e/alpha',
-    //  type: 'POST',
-    //  dataType: 'json',
-    //  data: {
-    //    secret: 'yunhua@926',
-    //  }
-    //});
 
-    var timer = setTimeout(function() {
+    var url = '/f2e/deployment/' + repo_id;
+    var timer;
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        secret: 'yunhua@926',
+        env_id: env_id,
+        branch: branch
+      },
+      success: function() {
+        publishing = false;
+      },
+      error: function() {
+        console.log(env_name, arguments);
+        $progress.text(env_name + '环境发布失败');
+
+        if (timer) {
+          clearTimeout(timer);
+        }
+
+        publishing = false;
+      }
+    });
+
+    $progress.parent().removeClass('hidden');
+
+    function get_progress() {
       $.ajax({
-        url: url,
+        url: '/f2e/deployment/' + repo_id + '/progress',
         type: 'GET',
         dataType: 'json',
         data: {
-          secret: 'yunhua@926'
+          secret: 'yunhua@926',
+          env: env_alias,
+          branch: branch,
+          _: Date.now()
         },
         success: function(res) {
-          if (res.fail) {
+          if (timer && res.fail) {
             clearTimeout(timer);
             publishing = false;
             return $progress.text('发布失败');
@@ -46,14 +75,21 @@ $(function() {
           if (res.progress === 100) {
             clearTimeout(timer);
             publishing = false;
+            return;
           }
+
+          timer = setTimeout(get_progress, 3000);
         },
         error: function(res) {
-          clearTimeout(timer);
+          if (timer) {
+            clearTimeout(timer);
+          }
           publishing = false;
           alert('服务器发生错误: ' + res.msg);
         }
       });
-    }, 1000);
+    }
+
+    get_progress();
   });
 });
