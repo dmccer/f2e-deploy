316e73ca577840c889b1acda122747e71dae733c
Kane <dmccer@gmail.com>
2015-08-27 23:24:59 +0800

    config gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 var PUB_DIR = './public';
 var CSS_DIR = path.join(PUB_DIR, 'css');
@@ -10,8 +12,13 @@ var JS_DIR = path.join(PUB_DIR, 'js');
 
 gulp.task('less', function() {
   return gulp
-    .src(['./static/less/**.*'])
+    .src(['./static/less/global/global.less', './static/less/page/*.less'])
     .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'ie 9', 'iOS 4', 'Android 4'],
+      cascade: false
+    }))
     .pipe(gulp.dest(CSS_DIR));
 });
 
@@ -20,6 +27,10 @@ gulp.task('js', function() {
     .src(['./static/js/**/*.js'])
     .pipe(uglify())
     .pipe(gulp.dest(JS_DIR));
+});
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch(['./static/**/*'], ['default']);
 });
 
 gulp.task('default', ['less', 'js']);
