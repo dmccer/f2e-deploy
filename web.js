d73d99ab7a07dfe2ee06d93321e7dc747309a30d
Kane <dmccer@gmail.com>
2015-08-29 18:21:04 +0800

    登录 session 控制

--- web.js
+++ web.js
@@ -4,8 +4,16 @@
 var express = require('express');
 var router = express.Router();
 var index = require('./controller/index');
+var sign = require('./controller/sign');
 
-router.get('/', index.list);
-router.get('/repos/:owner/:name', index.repo);
+router.get('/', sign.require_user, function(req, res) {
+  res.redirect('/repos');
+});
+router.get('/login', sign.show_login);
+router.post('/login', sign.login);
+router.get('/logout', sign.log_off);
+
+router.get('/repos', sign.require_user, index.list);
+router.get('/repos/:owner/:name', sign.require_user, index.repo);
 
 module.exports = router;
